92cd26597d135bcd68d91b10e5a6c694b9c8a339
Ho Bao An <hobaoan@hos-macbook-pro.local>
2023-09-16 23:51:20 +0700

    inital commit

---
+++ automation/test_compare.py
@@ -0,0 +1,17 @@
+
+# https://www.tutorialspoint.com/pytest/pytest_grouping_the_tests.htm
+import pytest
+@pytest.mark.great
+def test_greater():
+   num = 100
+   assert num > 100
+
+@pytest.mark.great
+def test_greater_equal():
+   num = 100
+   assert num >= 100
+
+@pytest.mark.others
+def test_less():
+   num = 100
+   assert num < 200
